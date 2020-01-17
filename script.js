ddcbb0063c7650c59511bc7bf7e0f763a2b644ed
Alex Joel <alexjoeldelgado@gmail.com>
2020-01-17 02:31:10 -0500

    Updated Javascript and added If loops to validate user input

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-        var specialCharacters = ["!#$%&'()*+,-./:;<=>?@[]^_`{|}~"];
-        var numericCharacters = ["1234567890"];
-        var uppercaseCharacters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-        var lowercaseCharacters = ["abcdefghijklmnopqrstuvwxyz"];
-        var passwordCharacterSet = "";
-        var retVal = "";
+var specialCharacters = ["!#$%&'()*+,-./:;<=>?@[]^_`{|}~"];
+var numericCharacters = ["1234567890"];
+var uppercaseCharacters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var lowercaseCharacters = ["abcdefghijklmnopqrstuvwxyz"];
 
-        document.getElementById("generate").addEventListener("click", function() {
-        function generatePassword(){
-        do {
-            var passwordLength = prompt("How long would you like your password to be? Please choose a number between 8 and 128.");
+
+document.getElementById("generate").addEventListener("click", function () {
+    function generatePassword() {
+        var retVal = "";
+        passwordCharacterSet = "";
+        var passwordLength = prompt("How long would you like your password to be? Please choose a number between 8 and 128.");
+        if (passwordLength < 8 || passwordLength > 128) {
+        alert("Please choose a number between 8 and 128!");
+        return generatePassword();
         }
-        while ((parseInt(passwordLength) < 8) || (parseInt(passwordLength) > 128) || (parseInt(passwordLength) === null));
-        
         var passwordSpecialCharacters = confirm("Would you like to include Special Characters?");
         if (passwordSpecialCharacters) {
             passwordCharacterSet = passwordCharacterSet + specialCharacters
@@ -28,7 +29,12 @@
         if (passwordLowercaseCharacters) {
             passwordCharacterSet = passwordCharacterSet + lowercaseCharacters
         }
+        if (!passwordSpecialCharacters && !passwordNumericCharacters && !passwordUppercaseCharacters && !passwordLowercaseCharacters) {
+        alert("Please choose a character set!")
+        return generatePassword();
+    }
 
+// add an if statement checking if charcterset includes undefined if so return to function
         console.log(passwordLength);
         console.log(passwordSpecialCharacters);
         console.log(passwordNumericCharacters);
@@ -37,21 +43,22 @@
         console.log(passwordCharacterSet);
 
         for (var i = 0; i < passwordLength; ++i) {
-        retVal += (passwordCharacterSet.charAt(Math.floor(Math.random() * passwordCharacterSet.length)));
+            retVal += (passwordCharacterSet.charAt(Math.floor(Math.random() * passwordCharacterSet.length)));
         }
         return retVal;
-    }    
-        generatePassword();
-        console.log(retVal);
-        document.getElementById("passwordResult").innerHTML = retVal;
-;})
+    }
+    passwordCharacterSet = "";
+    var retVal = generatePassword();
+    console.log(retVal);
+    document.getElementById("passwordResult").innerHTML = retVal;
+})
 function copyText() {
     var copyText = document.getElementById("passwordResult");
     copyText.select();
     copyText.setSelectionRange(0, 99999)
     document.execCommand("copy");
     alert("Copied the text: " + copyText.value);
-  }
-document.getElementById("copyText").addEventListener("click", function() {
+}
+document.getElementById("copyText").addEventListener("click", function () {
     copyText();
-})   
+})
